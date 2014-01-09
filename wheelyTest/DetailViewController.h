cca8ba71a009da455dcebc2cf4bc138b8800d5fa
Artem Abramov <aabramov@mera.ru>
2014-01-09 13:49:36 +0400

    Initial Commit

---
+++ wheelyTest/DetailViewController.h
@@ -0,0 +1,16 @@
+//
+//  DetailViewController.h
+//  wheelyTest
+//
+//  Created by Artem Abramov on 1/9/14.
+//  Copyright (c) 2014 Artem Abramov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) id detailItem;
+
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+@end
---
+++ wheelyTest/MasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  MasterViewController.h
+//  wheelyTest
+//
+//  Created by Artem Abramov on 1/9/14.
+//  Copyright (c) 2014 Artem Abramov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+@end
